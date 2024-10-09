93c856846f890d35b7ba28f3005395350854bd08
Andrey <rainman1811@gmail.com>
2024-10-09 21:39:44 +0300

    Add new test and new page

--- src/test/java/configs/TestSuiteElementCheck.java
+++ src/test/java/configs/TestSuiteElementCheck.java
@@ -1,4 +1,5 @@
 package configs;
+
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,12 @@ public class TestSuiteElementCheck {
         this.driver = new PlaywrightDriver();
         Page page = driver.getPage();
         this.pageStorage = new PageStorage(page);
+
     }
 
     @Test
     public void test_home_page() {
+
         var checkTitle = pageStorage.getHomePage().title_page();
         assert checkTitle.isVisible();
 
@@ -24,8 +27,44 @@ public class TestSuiteElementCheck {
         var selectLeftPanelButton = pageStorage.getHomePage().left_panel_buttons();
         selectLeftPanelButton.getFirst().click();
 
+        // Генерируем данные для заполнения полей
+        String generatedFullName = DataGenerator.FullName();
+        String generatedEmail = DataGenerator.Email();
+        String generatedCurrentAddress = DataGenerator.CurrentAddress();
+        String generatedPermanentAddress = DataGenerator.PermanentAddress();
 
+        var inputFullName = pageStorage.getTextBoxPage().userFullName();
+        inputFullName.fill(generatedFullName);
 
+        var inputEmail = pageStorage.getTextBoxPage().userEmail();
+        inputEmail.fill(generatedEmail);
+
+        var inputCurrentAddress = pageStorage.getTextBoxPage().userCurrentAddress();
+        inputCurrentAddress.fill(generatedCurrentAddress);
+
+        var inputPermanentAddress = pageStorage.getTextBoxPage().userPermanentAddress();
+        inputPermanentAddress.fill(generatedPermanentAddress);
+
+        var clickButtonSubmit = pageStorage.getTextBoxPage().submitButton();
+        clickButtonSubmit.click();
+
+        var checkResults = pageStorage.getTextBoxPage().outputField();
+        for (var result : checkResults) {
+            String textResult = result.textContent();
+
+            // Проверяем каждую строку на наличие сгенерированных данных
+            if (textResult.contains(generatedFullName)) {
+                System.out.println("Полное имя совпадает: " + textResult);
+            } else if (textResult.contains(generatedEmail)) {
+                System.out.println("Email совпадает: " + textResult);
+            } else if (textResult.contains(generatedCurrentAddress)) {
+                System.out.println("Текущий адрес совпадает: " + textResult);
+            } else if (textResult.contains(generatedPermanentAddress)) {
+                System.out.println("Постоянный адрес совпадает: " + textResult);
+            } else {
+                System.out.println("Найден неизвестный результат: " + textResult);
+            }
+        }
     }
 
     @AfterEach
