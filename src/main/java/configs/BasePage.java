6460636c96226ce2e03c4f06589af35f2c9cc5f9
Andrey <rainman1811@gmail.com>
2024-10-09 22:54:00 +0300

    add class logger

--- src/main/java/configs/BasePage.java
+++ src/main/java/configs/BasePage.java
@@ -17,18 +17,22 @@ public class BasePage {
     }
 
     public Locator get_element(String locator) {
+        LogManager.info("Получение элемента по locator" + locator);
         return page.locator(locator);
     }
 
     public List<Locator> get_elements(String locator) {
+        LogManager.info("Получение элементов по локатору: " + locator);
         return page.locator(locator).all();
     }
 
     public Locator get_by_role(AriaRole role, String name) {
+        LogManager.info("Получение элемента по роли: " + role + " и имени: " + name);
         return page.getByRole(role, new Page.GetByRoleOptions().setName(name));
     }
 
     public Locator get_by_text(String text) {
+        LogManager.info("Получение элемента по тексту: " + text);
         return page.getByText(text);
     }
 }
