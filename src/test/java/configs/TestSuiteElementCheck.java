18268a22e396ed0a67c741aa1a42313bb3470693
Andrey <rainman1811@gmail.com>
2024-10-10 19:09:24 +0300

    add allure

--- src/test/java/configs/TestSuiteElementCheck.java
+++ src/test/java/configs/TestSuiteElementCheck.java
@@ -3,6 +3,7 @@ package configs;
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class TestSuiteElementCheck {
     private final PlaywrightDriver driver;
@@ -16,6 +17,7 @@ public class TestSuiteElementCheck {
     }
 
     @Test
+    @DisplayName("Проверяем переход по сайту и заполнения формы.")
     public void testElementTextBox() {
 
         var checkTitle = pageStorage.getHomePage().title_page();
