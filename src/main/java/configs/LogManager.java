990860e3719cdb3f2efe81f0c4e94e1e203d912a
Andrey <rainman1811@gmail.com>
2024-10-10 18:55:03 +0300

    Added colors for logging. Added try catch to the base class

--- src/main/java/configs/LogManager.java
+++ src/main/java/configs/LogManager.java
@@ -3,24 +3,29 @@ package configs;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.fusesource.jansi.AnsiConsole;
+
 public class LogManager {
     private static final Logger logger = Logger.getLogger(LogManager.class.getName());
 
     private LogManager() {
-
+        AnsiConsole.systemInstall();
     }
 
     public static void info(String message) {
-        logger.log(Level.INFO, message);
+        logger.log(Level.INFO, "\u001B[32m" + message + "\u001B[0m"); // Зеленый
     }
 
     public static void warning(String message) {
-        logger.log(Level.WARNING, message);
+        logger.log(Level.WARNING, "\u001B[33m" + message + "\u001B[0m"); // Желтый
     }
 
     public static void error(String message) {
-        logger.log(Level.SEVERE, message);
+        logger.log(Level.SEVERE, "\u001B[31m" + message + "\u001B[0m"); // Красный
     }
 
+    public static void close() {
+        AnsiConsole.systemUninstall(); // Отключаем поддержку ANSI
+    }
 }
 
