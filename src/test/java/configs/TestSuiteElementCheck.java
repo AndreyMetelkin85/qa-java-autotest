160568591d3df121373130a5445a7168ceb40c33
Andrey <rainman1811@gmail.com>
2024-10-13 21:09:59 +0300

    add new class and fix test

--- src/test/java/configs/TestSuiteElementCheck.java
+++ src/test/java/configs/TestSuiteElementCheck.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 public class TestSuiteElementCheck {
     private final PlaywrightDriver driver;
     private final PageStorage pageStorage;
+    private final CustomExpect expect;
 
     public TestSuiteElementCheck() {
         this.driver = new PlaywrightDriver();
         Page page = driver.getPage();
         this.pageStorage = new PageStorage(page);
+        this.expect = new CustomExpect(page);
 
     }
 
@@ -21,7 +23,7 @@ public class TestSuiteElementCheck {
     public void testElementTextBox() {
 
         var checkTitle = pageStorage.getHomePage().title_page();
-        assert checkTitle.isVisible();
+        expect.toBeVisible(checkTitle, 2000);
 
         var selectCategory = pageStorage.getHomePage().category_cards_home_page();
         selectCategory.getFirst().click();
